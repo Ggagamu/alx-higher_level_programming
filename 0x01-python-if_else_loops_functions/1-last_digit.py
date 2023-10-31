56d1e36cfcdb59586a97e9e2197dbc70c1ccf0d5
Isaac <mugagga10@gmail.com>
2023-10-31 07:18:04 -0700

    check 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number < 0:
+    ran = (number * -1) % 10 * -1
+else:
+    ran = number % 10
+print ("Last digit of {}".format(number), end=' ')
+if ran > 5:
+    print("is {} and is greater than 5".format(ran))
+elif ran == 0:
+    print("is {} and is 0".format(ran))
+elif ran < 6 and num != 0:
+    print("is {} and is less than 6 and not 0".format(ran))
