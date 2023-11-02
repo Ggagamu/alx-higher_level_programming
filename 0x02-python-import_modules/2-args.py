0bb66c293e030d18b240ddc0e636cadf35b7f1af
Isaac <mugagga10@gmail.com>
2023-11-02 10:53:23 -0700

    check 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,15 @@
 if __name__ == "__main__":
     from sys import argv
 
-argc = len(argv) - 1
+argc = len(argv)
 
-if argc == 0:
-    print("No arguments passed.")
-elif argc == 1:
-    print("1 argument passed:")
+argc = len(argv)
+if argc < 2:
+    print("{} arguments.".format(argc - 1))
 else:
-    print("{} arguments passed:".format(argc))
-
-for i, arg in enumerate(argv[1:], start=1):
-    print("{}: {}".format(i, arg))
+    if argc == 2:
+        print("{} argument:".format(argc - 1))
+    else:
+        print("{} arguments:".format(argc - 1))
+    for n in range(1, argc):
+        print("{}: {}".format(n, argv[n]))
