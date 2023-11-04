c065ee12c182a2852eabd5c6053d4712a0ed6908
Isaac <mugagga10@gmail.com>
2023-11-04 08:35:33 -0700

    check six

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        return None
-    else:
         for row in matrix:
-            for digit in row:
-                print("{:d}".format(digit), end=" "
-                        if digit != row[-1] else '\n')
+            for dgt in row:
+                print("{:d}".format(dgt), end=" "
+                      if dgt != row[-1] else '\n')
+    else:
         print()
