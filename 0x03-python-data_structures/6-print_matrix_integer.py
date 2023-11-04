5578d5a7e3e9f1130a132b2d05ad0bb343039246
Isaac <mugagga10@gmail.com>
2023-11-04 08:37:39 -0700

    check sixx

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
+    if matrix != [[]]:
         for row in matrix:
             for dgt in row:
                 print("{:d}".format(dgt), end=" "
