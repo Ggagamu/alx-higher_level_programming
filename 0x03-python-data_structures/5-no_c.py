df9309b1269f11604b71d80cfdbf345a009d57e5
Isaac <mugagga10@gmail.com>
2023-11-04 07:23:55 -0700

    check 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if not my_string:
+        return None
+    else:
+        new_string = []
+        for char in my_string:
+            if char.lower() != 'c' or char.upper() != 'C':
+                new_string.append(char)
+        return ''.join(new_string)
