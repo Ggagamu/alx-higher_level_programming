d75e3aab30f83a1c75220979d7f685b498f35e4d
Isaac <mugagga10@gmail.com>
2024-02-12 09:57:53 -0800

    3rd batch

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const No. = process.argv[2];
+
+function factorial (No.) {
+  if (No. <= 1 || isNaN(No.)) {
+    return 1;
+  } else {
+    return No. * factorial(No. - 1);
+  }
+}
+
+console.log(factorial(No.));
