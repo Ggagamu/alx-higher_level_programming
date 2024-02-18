6b3f79854f3a4108acf1f4e635db9516e584c33c
Isaac <mugagga10@gmail.com>
2024-02-18 12:09:04 -0800

    checking all

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Modile for a script thatlists all states with a name starting with upper N from the database hbtn_0e_0_usa"""
+"""Module for a script that lists all states with a name 
+starting with upper N from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
 
