3877c5278c6a38092236ad425d9cefc7065223fc
Isaac <mugagga10@gmail.com>
2024-02-18 10:19:49 -0800

    1st batch

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,10 +3,10 @@
 
 if __name__ == '__main__':
 
-    import MySQLdb
+    import MySQLdb as Mdb
     import sys
 
-    db = MySQLdb.connect(host='localhost', port=3306,
+    db = Mdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
