c75083d71bea93d6b852f6aa7d81e677a753a039
Isaac <mugagga10@gmail.com>
2024-03-02 14:30:02 -0800

    correction

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!
-curl -s 0.0.0.0:5000/catch_me tee /dev/null | grep -o 'You got me!'
+#  Bash script that makes a request to 0.0.0.0:5000/catch_me  response "You got me!"
+curl -sX PUT -L -d "user_id=98" --header "origin: X-School" 0.0.0.0:5000/catch_me
